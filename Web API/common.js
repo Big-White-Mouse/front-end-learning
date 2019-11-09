7d1f8d3ba9ecc3bb8c76a2f9560b96b907af2fe2
dabaishu <1250709351@qq.com>
2019-11-09 22:04:22 +0800

    done!

--- Web API/common.js
+++ Web API/common.js
@@ -81,6 +81,7 @@ function getScroll(){
 }
 
 
+// 获取鼠标在页面的位置
 //通过页面滚动来处理pageX的兼容性问题
 //使用时注意传递参数
 //例如：
@@ -95,5 +96,56 @@ function getPage(e){
   return {
     pageX: pageX,
     pageY: pageY
+  }
+}
+
+
+//格式化日期对象，返回yyyy-MM-dd HH:mm:ss的形式
+function formatDate(date) {
+  // 判断参数date是否是日期对象
+  // instanceof  instance 实例(对象)   of 的
+  // console.log(date instanceof Date);
+  if (!(date instanceof Date)) {
+    console.error('date不是日期对象')
+    return;
+  }
+
+  var year = date.getFullYear(),
+      month = date.getMonth() + 1,
+      day = date.getDate(),
+      hour = date.getHours(),
+      minute = date.getMinutes(),
+      second = date.getSeconds();
+
+  month = month < 10 ? '0' + month : month;
+  day = day < 10 ? '0' + day : day;
+  hour = hour < 10 ? '0' + hour : hour;
+  minute = minute < 10 ? '0' + minute : minute;
+  second = second < 10 ? '0' + second : second;
+
+  return year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+}
+
+// 获取两个日期的时间差
+function getInterval(start, end) {
+  // 两个日期对象，相差的毫秒数
+  var interval = end - start;
+  // 求 相差的天数/小时数/分钟数/秒数
+  var day, hour, minute, second;
+
+  // 两个日期对象，相差的秒数
+  // interval = interval / 1000;
+  interval /= 1000;
+
+  day = Math.round(interval / 60 / 60 / 24);
+  hour = Math.round(interval / 60 / 60 % 24);
+  minute = Math.round(interval / 60 % 60);
+  second = Math.round(interval % 60);
+
+  return {
+    day: day,
+    hour: hour,
+    minute: minute,
+    second: second
   }
 }
